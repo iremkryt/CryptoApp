13c545022fc5c1cebb256952053adb2e5eb455e1
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-04-07 11:17:32 +0300

    Created coinBinding

---
+++ app/src/main/java/com/iremkryt/cryptoapp/bindingadapters/CoinBinding.kt
@@ -0,0 +1,19 @@
+package com.iremkryt.cryptoapp.bindingadapters
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.iremkryt.cryptoapp.utils.loadImage
+
+class CoinBinding {
+
+    companion object{
+        //https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png
+
+        @BindingAdapter("load_image")
+        @JvmStatic
+        fun loadImage(imageView: ImageView, coinImage: String){
+            val imageUrl = "https://s2.coinmarketcap.com/static/img/coins/64x64/$coinImage.png"
+            imageView.loadImage(imageUrl)
+        }
+    }
+}
