cb54d3e1b60cf5b2cb77979eb27d68cd050aece3
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-04-10 19:09:03 +0300

    Deleted some unnecessary things

--- app/src/main/java/com/iremkryt/cryptoapp/model/home/Data.kt
+++ app/src/main/java/com/iremkryt/cryptoapp/model/home/Data.kt
@@ -4,22 +4,14 @@ package com.iremkryt.cryptoapp.model.home
 import com.google.gson.annotations.SerializedName
 
 data class Data(
-    @SerializedName("circulating_supply")
-    val circulatingSupply: Double?,
-    @SerializedName("cmc_rank")
-    val cmcRank: Int?,
     @SerializedName("date_added")
     val dateAdded: String?,
     @SerializedName("id")
     val id: Int?,
     @SerializedName("last_updated")
     val lastUpdated: String?,
-    @SerializedName("max_supply")
-    val maxSupply: Long?,
     @SerializedName("name")
     val name: String?,
-    @SerializedName("num_market_pairs")
-    val numMarketPairs: Int?,
     @SerializedName("platform")
     val platform: Platform?,
     @SerializedName("quote")
@@ -34,8 +26,4 @@ data class Data(
     val symbol: String?,
     @SerializedName("tags")
     val tags: List<String?>?,
-    @SerializedName("total_supply")
-    val totalSupply: Double?,
-    @SerializedName("tvl_ratio")
-    val tvlRatio: Any?
 )
