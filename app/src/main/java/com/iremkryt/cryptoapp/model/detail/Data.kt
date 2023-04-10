cf1936d5a84dd1322e5f279d916ed3f01b9423e4
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-04-10 19:07:19 +0300

    Created detail api

---
+++ app/src/main/java/com/iremkryt/cryptoapp/model/detail/Data.kt
@@ -0,0 +1,9 @@
+package com.iremkryt.cryptoapp.model.detail
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Data(
+    @SerializedName("ETH")
+    val eTH: List<CoinDetail?>?
+)
