9a83e2dc36148b74c6470c85fc2fbb5f01569ad8
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-04-11 23:11:21 +0300

    Updated homefragment

--- app/src/main/java/com/iremkryt/cryptoapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/iremkryt/cryptoapp/ui/home/HomeFragment.kt
@@ -1,8 +1,14 @@
 package com.iremkryt.cryptoapp.ui.home
 
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.Navigation
 import com.iremkryt.cryptoapp.base.BaseFragment
 import com.iremkryt.cryptoapp.databinding.FragmentHomeBinding
+import com.iremkryt.cryptoapp.model.home.Data
 import com.iremkryt.cryptoapp.utils.Constant.API_KEY
 import com.iremkryt.cryptoapp.utils.Constant.LIMIT
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,8 +19,12 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(
 ) {
     override val viewModel by viewModels<HomeViewModel>()
 
-    override fun onCreateFinished() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         viewModel.getData(API_KEY, LIMIT)
+    }
+    override fun onCreateFinished() {
+        //viewModel.getData(API_KEY, LIMIT)
     }
 
     override fun initializeListeners() {
@@ -22,7 +32,35 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(
     }
 
     override fun observeEvents() {
-
+        with(viewModel){
+            cryptoResponse.observe(viewLifecycleOwner, Observer {
+                it?.let {
+                    it.data?.let { it1 -> setRecycler(it1 as List<Data>) }//HATALI OLABİLİR
+                }
+            })
+            isLoading.observe(viewLifecycleOwner, Observer {
+                handleViews(it)
+            })
+            onError.observe(viewLifecycleOwner, Observer {
+                Toast.makeText(requireContext(), it, Toast.LENGTH_LONG).show()
+            })
+        }
     }
 
+    private fun setRecycler(data: List<Data>){
+        val mAdapter = HomeRecyclerAdapter(object : ItemClickListener{
+            override fun onItemClick(coin: Data) {
+                if(coin.symbol != null){
+                    val navigation = HomeFragmentDirections.actionHomeFragmentToDetailFragment(coin.symbol)
+                    Navigation.findNavController(requireView()).navigate(navigation)
+                }
+            }
+        })
+        binding.rvHome.adapter = mAdapter
+        mAdapter.setList(data)
+    }
+    private fun handleViews(isLoading: Boolean = false){
+        binding.rvHome.isVisible = !isLoading
+        binding.pbHome.isVisible = isLoading
+    }
 }
